3ee780054c1ee400b6caeb7bfa8b41eec9026f3b
osmartumeflores <osmar.tume@ucsm.edu.pe>
2026-04-25 01:52:18 -0500

    Actualizacion de productos

--- tienda-virtual/src/data/products.js
+++ tienda-virtual/src/data/products.js
@@ -1,73 +1,73 @@
 export const products = [
   {
     id: 1,
-    name: "Laptop Pro 15",
+    name: "Laptop Vivo Book Pro 15",
     price: 1299.99,
     category: "Electrónica",
-    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&h=500&fit=crop",
+    image: "https://dlcdnwebimgs.asus.com/gain/7314a203-99d6-4e68-bb5e-c8e6f844b29e/",
     description: "Potente laptop para profesionales y desarrolladores",
     stock: 10
   },
   {
     id: 2,
-    name: "Smartphone X100",
+    name: " Samsung Galaxy S26 Ultra 12gb 256 Gb Black",
     price: 799.99,
     category: "Electrónica",
-    image: "https://images.unsplash.com/photo-1511707267537-b85faf00021e?w=500&h=500&fit=crop",
+    image: "https://http2.mlstatic.com/D_NQ_NP_938583-MLA106978597200_022026-O.webp",
     description: "Smartphone de última generación con cámara 5G",
     stock: 15
   },
   {
     id: 3,
-    name: "Audífonos Inalámbricos",
+    name: "Audífonos Gamer Redragon Zeus X H510-RGB 7.1 VirtuaL",
     price: 199.99,
     category: "Audio",
-    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&h=500&fit=crop",
+    image: "https://promart.vteximg.com.br/arquivos/ids/6731573/image-5790dc68b8324336a60f143a59e38a00.jpg?v=638109710044500000",
     description: "Audífonos premium con cancelación de ruido",
     stock: 20
   },
   {
     id: 4,
-    name: "Tablet 12 inches",
+    name: "Galaxy Tab S8 Ultra 14.6 Pulgadas 12gb 256 Gb Black",
     price: 499.99,
     category: "Electrónica",
-    image: "https://images.unsplash.com/photo-1561070791-2526d30994b5?w=500&h=500&fit=crop",
+    image: "https://http2.mlstatic.com/D_NQ_NP_710288-MLU77288145494_072024-O.webp",
     description: "Tablet grande perfecta para trabajar y entretenimiento",
     stock: 8
   },
   {
     id: 5,
-    name: "Cámara Digital 4K",
+    name: "GoPro Hero 7 Black",
     price: 899.99,
     category: "Fotografía",
-    image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=500&h=500&fit=crop",
+    image: "https://prizmastore.pe/wp-content/uploads/2019/03/gopro-hero-7-black.jpg",
     description: "Cámara profesional con grabación en 4K",
     stock: 5
   },
   {
     id: 6,
-    name: "Smartwatch Deportivo",
+    name: "Samsung Galaxy Watch Ultra",
     price: 349.99,
     category: "Wearables",
-    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&h=500&fit=crop",
+    image: "https://http2.mlstatic.com/D_NQ_NP_991565-MLU78338681508_082024-O.webp",
     description: "Reloj inteligente con sensor de frecuencia cardíaca",
     stock: 12
   },
   {
     id: 7,
-    name: "Monitor 4K 32 inches",
+    name: "Monitor Xiaomi Gaming G27i 2026 27 Panel Fast IPS FHD 1920x1080 200Hz 1ms HDR400",
     price: 599.99,
     category: "Periféricos",
-    image: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?w=500&h=500&fit=crop",
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ5Huy5h-N4E5QLS3UfHqjbJPZX3QG1r6uHLVBGUy-ePnePUJCJVYgyqmCRMa1VcVw2wpmphRjkulQUBHgr_T3VbCq_KZeZUJkbFtJ4-PwFO-frrqOchBV4gLSskR-aUeZ54OYEUGy2iqs&usqp=CAc",
     description: "Monitor ultrapanorámico para profesionales",
     stock: 6
   },
   {
     id: 8,
-    name: "Teclado Mecánico RGB",
+    name: "Teclado Mecánico Redragon K552 Led 87 Teclas Negro",
     price: 149.99,
     category: "Periféricos",
-    image: "https://images.unsplash.com/photo-1587829191301-4f3d310ca713?w=500&h=500&fit=crop",
+    image: "https://http2.mlstatic.com/D_NQ_NP_701641-MLA100047315677_122025-O.webp",
     description: "Teclado mecánico gaming con iluminación RGB",
     stock: 25
   }
